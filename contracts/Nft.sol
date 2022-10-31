465fd16a8e78b8aaf498af3d7c6cef10ebfa89aa
lishiyao <40553790+nextniko@users.noreply.github.com>
2022-10-31 18:19:24 +0800

    feat(contract): update Nft.sol SBTNFTAddressLists

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -13,11 +13,18 @@ contract GameItem is ERC721URIStorage {
     Counters.Counter private _tokenIds;
 
     bool isSBT = true;
+
     uint256 MintMaxTotal = 1000;
+
     string baseURI;
+
     string contractMetadata;
+
     address owner;
+
     mapping(address => bool) public whiteLists;
+
+    mapping(address => string) public SBTNFTAddressLists;
 
     constructor(string memory _baseURI, string memory _contractMetadata) ERC721("GameItem", "ITM") {
         baseURI = _baseURI;
@@ -25,22 +32,20 @@ contract GameItem is ERC721URIStorage {
         owner = msg.sender;
     }
 
-    function mint(address player,string memory tokenURI)
+    function mint(address player)
         public
         returns (uint256)
     {
         // require(whiteLists[player], "This address is not white");
-        
         uint256 newItemId = _tokenIds.current();
         // string memory tokenURI = getTokenURI(newItemId);
         require(MintMaxTotal >= newItemId, "Max overflow !");
         _mint(player, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-
+        _setTokenURI(newItemId, getStaticJsonTokenURI(0));
+        setSBTNFTAddressLists(player, getStaticJsonTokenURI(0));
         _tokenIds.increment();
         return newItemId;
     }
-
     function setTokenURI (uint256 itemId , string memory tokenURI )
         public
         byOwner()
@@ -49,7 +54,14 @@ contract GameItem is ERC721URIStorage {
         _setTokenURI(itemId, tokenURI);
         return itemId;
     }
-    
+
+    function setSBTNFTAddressLists (address _userAddress , string memory _tokenURI) public {
+        SBTNFTAddressLists[_userAddress] = _tokenURI;
+    }
+
+    function getSBTNFTAddressLists (address _userAddress) public view returns (string memory) {
+        return SBTNFTAddressLists[_userAddress];
+    }
 
     function setWhiteLists (address _userAddress , bool _whiteState) public byOwner(){
         whiteLists[_userAddress] = _whiteState;
@@ -59,17 +71,36 @@ contract GameItem is ERC721URIStorage {
         return "https://raw.githubusercontent.com/nextniko/web3-Intelligent-contract/main/njl-nft.json";
     }
 
-    // function getTokenURI (uint256 index) private pure returns(string memory) {
-    //     string memory indexString = Strings.toString(index);
-    //     string memory headerString = "https://raw.githubusercontent.com/nextniko/web3-Intelligent-contract/main/";
-    //     string memory footerString = ".json";
-    //     string memory tokenURI = string.concat(
-    //         headerString,
-    //         indexString,
-    //         footerString
-    //     );
-    //     return tokenURI;
-    // }
+    function getHttpJsonTokenURI (uint256 index) private pure returns(string memory) {
+        string memory tokenURI = "https://raw.githubusercontent.com/nextniko/web3-Intelligent-contract/main/njl-nft-mint.json";
+        return tokenURI;
+    }
+
+    function getStaticJsonTokenURI (uint256 index) private pure returns(string memory) {
+        string memory tokenURI =  string(bytes(
+            abi.encodePacked(
+                "{",
+                    '"title":"nft-njl Metadata",',
+                    '"type":"object",',
+                    '"properties": {',
+                        '"name": {',
+                            '"type": "string",',
+                            '"description": "nft-njl-metadata-name"',
+                        '},',
+                        '"description": {',
+                            '"type": "string",',
+                            '"description": "nft-njl-metadata-description"',
+                        '},',
+                        '"image": {',
+                            '"type": "string",',
+                            '"description": "https://docs.openzeppelin.com/_/images/social.png"',
+                        '}',
+                    '}',
+                "}"
+            )
+        ));
+        return tokenURI;
+    }
 
     modifier byOwner(){
         require(msg.sender == owner, "Not owner!");
